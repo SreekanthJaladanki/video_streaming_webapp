b0cadf4a97cdd6a5119003f8bfc9ccb9ae474751
Sreekanth <sreekanthjaladanki@gmail.com>
2024-06-24 18:19:19 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,22 @@
+const video = document.getElementById('videoPlayer');
+
+function playPause() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function stopVideo() {
+    video.pause();
+    video.currentTime = 0;
+}
+
+function skip(value) {
+    video.currentTime += value;
+}
+
+function setVolume(value) {
+    video.volume = value;
+}
